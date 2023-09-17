1dd41381e4cccfb48aea0ce94f7539b3aae21fef
Mira <mira.theidel@gmx.de>
2023-09-17 11:18:50 +0200

    small adjustments before introducing dev branch

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -31,11 +31,13 @@ def set_page_settings():
 def initialize_session(refresh_session=False):
     if refresh_session:
         st.session_state.messages = []
-        st.session_state.openai_lc_client = None
-        st.session_state.result = None
+        #st.session_state.openai_lc_client = None
+        #st.session_state.result = None
     else:
         if "messages" not in st.session_state:
             st.session_state.messages = []
+        if "data_file" not in st.session_state:
+            st.session_state.data_file = None
         if "costs" not in st.session_state:
             st.session_state.costs = []
         if "total_tokens" not in st.session_state:
@@ -62,7 +64,7 @@ def display_sidemenu():
     st.sidebar.title('Menu')
     st.sidebar.markdown(
     """
-    Please uploade your file or enter a url.  Supported file types: 
+    Please uploade your file or enter a url. Supported file types: 
     """
     )
     col1, col2 = st.sidebar.columns(2)
@@ -103,13 +105,24 @@ def display_sidemenu():
 @register_page(MAIN_PAGE)
 def questionai():
     with st.container():
-        if prompt := st.chat_input("-> Post questions regarding the content of your file, AI will answer..."):
-            print(prompt)
-            st.session_state.messages.append({"role": "user", "content": prompt})
-            for message in st.session_state.messages:
+        
+
+        for message in st.session_state.messages:
                 #image = "xxx.png" if message["role"] == "user" else "xxx.png"
                 with st.chat_message(message["role"]):
                     st.markdown(message["content"])
+        
+
+        if prompt := st.chat_input("-> Post questions regarding the content of your file, AI will answer..."):
+            # print(prompt)
+            st.chat_message("user").markdown(prompt)
+            st.session_state.messages.append({"role": "user", "content": prompt})
+
+            response = f"Echo: {prompt}" # TODO: connect to API response
+            
+            with st.chat_message("assistant"):
+                st.markdown(response)
+            st.session_state.messages.append({"role": "assistant", "content": response})
         else:
             image = Image.open('./static/main_picture3.png')
             st.image(image, caption=None)
