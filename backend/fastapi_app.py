aed41e1e0da01b45562daeb2c027018db7903a14
Mira <mira.theidel@gmx.de>
2023-10-02 23:17:30 +0200

    clear files in file_uploader on frontend and fix upload of combined uploadfile and str

--- backend/fastapi_app.py
+++ backend/fastapi_app.py
@@ -1,22 +1,23 @@
 # command to run: uvicorn fastapi_app:app --reload
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, Form, HTTPException, UploadFile
 from pydantic import BaseModel
 
-#from llama_index.callbacks import CallbackManager, TokenCountingHandler
-    
+# from llama_index.callbacks import CallbackManager, TokenCountingHandler
+
 import logging
 import sys
 from dotenv import load_dotenv
 import pathlib
 import os
 import certifi
-#import tiktoken
+
+# import tiktoken
 
 # Set-up Chat Engine: CondenseQuestionChatEngine with RetrieverQueryEngine
 from script import (
-    AITextDocument, 
+    AITextDocument,
     AIHtmlDocument,
-    CustomLlamaIndexChatEngineWrapper, 
+    # CustomLlamaIndexChatEngineWrapper,
     set_up_chatbot,
 )
 
@@ -35,18 +36,21 @@ chat_bot, callback_manager, token_counter = set_up_chatbot()
 
 app = FastAPI()
 
+
 class TextSummaryModel(BaseModel):
     file_name: str
     text_category: str
     summary: str
     used_tokens: int
 
+
 class QuestionModel(BaseModel):
     prompt: str
     temperature: float
 
-class UploadModel(BaseModel):
-    file: UploadFile | str
+
+class UrlUploadModel(BaseModel):
+    url: str
 
 
 class QAResponseModel(BaseModel):
@@ -54,47 +58,48 @@ class QAResponseModel(BaseModel):
     ai_answer: str
     used_tokens: int
 
+
 class TextResponseModel(BaseModel):
     message: str
 
 
 @app.post("/upload", response_model=TextSummaryModel)
-async def upload_file(upload: UploadModel):
-    if not upload:
-        return TextSummaryModel(
-            file_name="",
-            text_category="",
-            summary="No file/url was uploaded.",
-            used_tokens=0,
-        )
-    
+async def upload_file(
+    upload_file: UploadFile | None = None, upload_url: str = Form("")
+):
     token_counter.reset_counts()
     cfd = pathlib.Path(__file__).parent
     try:
-        if type(upload.file) == str:
-            document = AIHtmlDocument(upload.file,LLM_STR,callback_manager)
-            file_name = upload.file
-        else:
-            # Ensure that the data folder exists
+        if upload_file:
+            if upload_url:
+                raise HTTPException(
+                    status_code=400, detail="You can not provide both, file and URL."
+                )
             os.makedirs("data", exist_ok=True)
-            with open(cfd / "data" / upload.file.filename, "wb") as f:
-                f.write(upload.file.read())
-            document = AITextDocument(upload.file.filename, LLM_STR, callback_manager)
-            file_name = upload.file.filename
-        chat_bot.add_document(document)
+            file_name = upload_file.filename
+            with open(cfd / "data" / file_name, "wb") as f:
+                f.write(await upload_file.read())
+            document = AITextDocument(file_name, LLM_STR, callback_manager)
+        elif upload_url:
+            document = AIHtmlDocument(upload_url, LLM_STR, callback_manager)
+            file_name = upload_url
+        else:
+            raise HTTPException(
+                status_code=400,
+                detail="You must provide either a file or URL to upload.",
+            )
 
     except Exception as e:
         return TextSummaryModel(
-            file_name=file_name,
+            file_name="",
             text_category="",
             summary=f"There was an error on uploading the file: {e.args}",
             used_tokens=int(token_counter.total_llm_token_count),
         )
-    finally:
-        if type(upload.file) != str:
-            upload.file.close() # do I need this (with statement)?
-        
-    #logging.debug(document.text_summary)
+
+    chat_bot.add_document(document)
+
+    # logging.debug(document.text_summary)
     return TextSummaryModel(
         file_name=file_name,
         text_category=document.text_category,
@@ -102,21 +107,23 @@ async def upload_file(upload: UploadModel):
         used_tokens=token_counter.total_llm_token_count,
     )
 
+
 @app.post("/qa_text", response_model=QAResponseModel)
 async def qa_text(question: QuestionModel):
     token_counter.reset_counts()
-    #logging.debug(question.prompt)
+    # logging.debug(question.prompt)
     chat_bot.update_temp(question.temperature)
     response = chat_bot.chat_engine.chat(
         question.prompt,
     )
-    #logging.debug(response.response)
+    # logging.debug(response.response)
 
     return QAResponseModel(
-        user_question= question.prompt,
-        ai_answer= str(response),
+        user_question=question.prompt,
+        ai_answer=str(response),
         used_tokens=token_counter.total_llm_token_count,
     )
+
 
 @app.get("/clear_storage", response_model=TextResponseModel)
 async def clear_storage():
@@ -126,6 +133,7 @@ async def clear_storage():
         return TextResponseModel(message=f"Error: {e.args}")
     return TextResponseModel(message="Knowledge base succesfully cleared")
 
+
 @app.get("/clear_history", response_model=TextResponseModel)
 async def clear_history():
     try:
@@ -134,8 +142,10 @@ async def clear_history():
         return TextResponseModel(message=f"Error: {e.args}")
     return TextResponseModel(message="Chat history succesfully cleared")
 
+
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(
         "fastapi_app:app",
         host="0.0.0.0",
--- backend/script.py
+++ backend/script.py
@@ -1,8 +1,6 @@
-
-
 from llama_index import (
     SimpleWebPageReader,
-    VectorStoreIndex, 
+    VectorStoreIndex,
     SimpleDirectoryReader,
     ServiceContext,
     StorageContext,
@@ -24,7 +22,8 @@ from llama_index.query_engine import RetrieverQueryEngine
 from llama_index.chat_engine.condense_question import CondenseQuestionChatEngine
 from llama_index.callbacks import CallbackManager, TokenCountingHandler
 from llama_index.memory import ChatMemoryBuffer
-from llama_index.indices.vector_store.retrievers import VectorIndexRetriever
+
+# from llama_index.indices.vector_store.retrievers import VectorIndexRetriever
 from llama_index.vector_stores.types import MetadataInfo, VectorStoreInfo
 
 from marvin import ai_model
@@ -38,18 +37,27 @@ from document_categories import CATEGORY_LABELS
 
 class AITextDocument:
     """Loads and converts a text file into LlamaIndex nodes.
-       The marvin ai_model predicts the text category based on a 
-       given list and gives a short summary of the text based on the given llm_str. 
+    The marvin ai_model predicts the text category based on a
+    given list and gives a short summary of the text based on the given llm_str.
     """
 
     cfd = pathlib.Path(__file__).parent / "data"
 
-    def __init__(self, document_name: str, llm_str: str, callback_manager: CallbackManager | None =None):
+    def __init__(
+        self,
+        document_name: str,
+        llm_str: str,
+        callback_manager: CallbackManager | None = None,
+    ):
         self.callback_manager: CallbackManager | None = callback_manager
         self.document = self._load_document(document_name)
         self.nodes = self.split_document_and_extract_metadata(llm_str)
-        self.text_category = self.nodes[0].metadata["marvin_metadata"].get("text_category")
-        self.text_summary: str = self.nodes[0].metadata["marvin_metadata"].get("description")
+        self.text_category = ",".join(
+            node.metadata["marvin_metadata"].get("text_category") for node in self.nodes
+        )
+        self.text_summary: str = "".join(
+            node.metadata["marvin_metadata"].get("description") for node in self.nodes
+        )
 
     @classmethod
     def _load_document(cls, identifier: str):
@@ -57,80 +65,79 @@ class AITextDocument:
 
         identifier: name of the text file as str
         """
-        return(
-            SimpleDirectoryReader(
-                input_files=[str(AITextDocument.cfd / identifier)],
-                encoding="utf-8",
-            ).load_data()[0]
-        )
+        return SimpleDirectoryReader(
+            input_files=[str(AITextDocument.cfd / identifier)],
+            encoding="utf-8",
+        ).load_data()[0]
 
     def _get_text_splitter(self):
         return TokenTextSplitter(
-            separator=" ", 
-            chunk_size=1024, 
+            separator=" ",
+            chunk_size=1024,
             chunk_overlap=128,
-            callback_manager=self.callback_manager
+            callback_manager=self.callback_manager,
         )
-    
+
     def _get_metadata_extractor(self, llm_str):
         return MetadataExtractor(
             extractors=[
                 MarvinMetadataExtractor(
-                    marvin_model=AITextDocument.AIDocument, 
+                    marvin_model=AITextDocument.AIDocument,
                     llm_model_string=llm_str,
-                    show_progress = True,
+                    show_progress=True,
                     callback_manager=self.callback_manager,
-
                 ),
             ],
         )
-    
+
     def split_document_and_extract_metadata(self, llm_str):
         text_splitter = self._get_text_splitter()
         metadata_extractor = self._get_metadata_extractor(llm_str)
         node_parser = SimpleNodeParser(
             text_splitter=text_splitter,
             metadata_extractor=metadata_extractor,
-            callback_manager = self.callback_manager,
+            callback_manager=self.callback_manager,
         )
         return node_parser.get_nodes_from_documents([self.document])
 
-
     @ai_model
     class AIDocument(BaseModel):
-        description: str = Field(..., description="A brief summary of the document content in 5 sentences.")
-        text_category: str = Field(...,description=f"best matching text category from the following list: {str(CATEGORY_LABELS)}")
-    
+        description: str = Field(
+            ..., description="A brief summary of the document content in 5 sentences."
+        )
+        text_category: str = Field(
+            ...,
+            description=f"""best matching text category from the following list: 
+                {str(CATEGORY_LABELS)}
+            """,
+        )
+
+
 class AIHtmlDocument(AITextDocument):
-
-    # def __init__(self, url: str, llm_str: str, callback_manager: CallbackManager | None =None):
-    #     self.callback_manager: CallbackManager | None = callback_manager
-    #     self.document = self._load_html(url)
-    #     self.nodes = self.split_document_and_extract_metadata(llm_str)
-    #     self.text_category = self.nodes[0].metadata["marvin_metadata"].get("text_category")
-    #     self.text_summary: str = self.nodes[0].metadata["marvin_metadata"].get("description")
-
-    
     @classmethod
     def _load_document(cls, identifier: str):
         """loads the data of an html file at a given url
 
         identifier: url of the html file as str
         """
-        return(
-            SimpleWebPageReader(
-                html_to_text=True,
-            ).load_data([identifier])[0]   
-        )
-    
+        return SimpleWebPageReader(
+            html_to_text=True,
+        ).load_data(
+            [identifier]
+        )[0]
+
 
 class CustomLlamaIndexChatEngineWrapper:
-    
-    system_prompt: str = """You are a chatbot that responds to all questions about the given context. The user gives you instructions on which questions to answer. 
-    When you write the answers, you need to make sure that the user's expectations are met. Remember that you are an accurate and experienced writer 
+    system_prompt: str = """You are a chatbot that responds to all questions about the 
+    given context.
+    The user gives you instructions on which questions to answer. 
+    When you write the answers, you need to make sure that the user's expectations are 
+    met. 
+    Remember that you are an accurate and experienced writer 
     and you write unique answers. Don't add anything hallucinatory.
-    Use friendly, easy-to-read language, and if it is a technical or scientific text, please stay correct and focused.
-    Responses should be no longer than 10 sentences, unless the user explicitly specifies the number of sentences.
+    Use friendly, easy-to-read language, and if it is a technical or scientific text, 
+    please stay correct and focused. Responses should be no longer than 10 sentences, 
+    unless the user explicitly specifies the number of sentences.
     """
 
     OPENAI_MODEL = "gpt-3.5-turbo"
@@ -139,42 +146,48 @@ class CustomLlamaIndexChatEngineWrapper:
 
     def __init__(self, callback_manager=None):
         self.callback_manager = callback_manager
-        self.text_category: str = "" # default, if no document is loaded yet
+        self.text_category: str = ""  # default, if no document is loaded yet
         self.llm = OpenAI(
-            model=CustomLlamaIndexChatEngineWrapper.OPENAI_MODEL, 
-            temperature=0, 
-            max_tokens=512
+            model=CustomLlamaIndexChatEngineWrapper.OPENAI_MODEL,
+            temperature=0,
+            max_tokens=512,
         )
         self.service_context = self._create_service_context()
         set_global_service_context(self.service_context)
         self.documents = []
 
-        if any(pathlib.Path(CustomLlamaIndexChatEngineWrapper.cfd / "storage").iterdir()):
+        if any(
+            pathlib.Path(CustomLlamaIndexChatEngineWrapper.cfd / "storage").iterdir()
+        ):
             self.storage_context = StorageContext.from_defaults(
-                persist_dir= str(CustomLlamaIndexChatEngineWrapper.cfd / "storage"),
+                persist_dir=str(CustomLlamaIndexChatEngineWrapper.cfd / "storage"),
             )
-            self.vector_index = load_index_from_storage(storage_context=self.storage_context)
+            self.vector_index = load_index_from_storage(
+                storage_context=self.storage_context
+            )
         else:
             self.vector_index = self.create_vector_index()
-        self.chat_engine = self.create_chat_engine() 
-        
+        self.chat_engine = self.create_chat_engine()
+
     def _create_service_context(self):
-        return ServiceContext.from_defaults( 
-            chunk_size=1024, 
+        return ServiceContext.from_defaults(
+            chunk_size=1024,
             chunk_overlap=152,
             llm=self.llm,
             system_prompt=CustomLlamaIndexChatEngineWrapper.system_prompt,
             callback_manager=self.callback_manager,
         )
-    
-    def add_document(self, document:AITextDocument):
+
+    def add_document(self, document: AITextDocument):
         self.documents.append(document)
         self._add_to_vector_index(document.nodes)
-        self.text_category = document.text_category # TODO: find mojority, if multiple docs are loaded
+        self.text_category = (
+            document.text_category
+        )  # TODO: find mojority, if multiple docs are loaded
         self.vector_index.storage_context.persist(
             persist_dir=CustomLlamaIndexChatEngineWrapper.cfd / "storage"
         )
-    
+
     def empty_vector_store(self):
         doc_ids = list(self.vector_index.ref_doc_info.keys())
         for doc_id in doc_ids:
@@ -185,17 +198,19 @@ class CustomLlamaIndexChatEngineWrapper:
         self.documents.clear()
 
     def create_vector_index(self):
-        #print(node. for doc in self.documents for node in doc.nodes)
+        # print(node. for doc in self.documents for node in doc.nodes)
         return VectorStoreIndex(
-            [node for doc in self.documents for node in doc.nodes], # current use case: no docs availabe, so empty list []
+            [
+                node for doc in self.documents for node in doc.nodes
+            ],  # current use case: no docs availabe, so empty list []
             service_context=self.service_context,
             storage_context=self.storage_context,
-        ) # openai api is called with whole text to make the embeddings
-    
+        )  # openai api is called with whole text to make the embeddings
+
     def _add_to_vector_index(self, nodes):
         self.vector_index.insert_nodes(
-            nodes, 
-            #service_context=self.service_context) # is this enough or do I have to recreate the chat engine?
+            nodes,
+            # service_context=self.service_context)
         )
 
     def _create_vector_index_retriever(self):
@@ -205,7 +220,9 @@ class CustomLlamaIndexChatEngineWrapper:
                 MetadataInfo(
                     name="text_category",
                     type="str",
-                    description="best matching text category (e.g. Technical, Biagraphy, Sience Fiction, ... )",
+                    description="""best matching text category (e.g. Technical, 
+                        Biagraphy, Sience Fiction, ... )
+                    """,
                 ),
                 MetadataInfo(
                     name="description",
@@ -227,22 +244,25 @@ class CustomLlamaIndexChatEngineWrapper:
             callback_manager=self.callback_manager,
         )
         return CondenseQuestionChatEngine.from_defaults(
-            query_engine=vector_query_engine, 
-            #condense_question_prompt=custom_prompt,
-            #chat_history=custom_chat_history,
-            memory = ChatMemoryBuffer.from_defaults(token_limit=1500),
+            query_engine=vector_query_engine,
+            # condense_question_prompt=custom_prompt,
+            # chat_history=custom_chat_history,
+            memory=ChatMemoryBuffer.from_defaults(token_limit=1500),
             verbose=True,
             callback_manager=self.callback_manager,
         )
-    
+
     def clear_chat_history(self):
         self.chat_engine.reset()
 
     def update_temp(self, temperature):
         # see https://gpt-index.readthedocs.io/en/v0.8.34/examples/llm/XinferenceLocalDeployment.html
-        self.vector_index.service_context.llm.__dict__.update({'temperature':temperature})
-        #self.llm.temperature=temperature
-        
+        self.vector_index.service_context.llm.__dict__.update(
+            {"temperature": temperature}
+        )
+        # self.llm.temperature=temperature
+
+
 def set_up_chatbot():
     token_counter = TokenCountingHandler(
         tokenizer=tiktoken.encoding_for_model("gpt-3.5-turbo").encode
@@ -250,9 +270,7 @@ def set_up_chatbot():
     callback_manager = CallbackManager([token_counter])
 
     return (
-        CustomLlamaIndexChatEngineWrapper(
-            callback_manager=callback_manager
-        ),
+        CustomLlamaIndexChatEngineWrapper(callback_manager=callback_manager),
         callback_manager,
         token_counter,
     )
@@ -270,14 +288,14 @@ if __name__ == "__main__":
 
     load_dotenv()
 
-    #API_KEY = os.getenv('OPENAI_API_KEY')
+    # API_KEY = os.getenv('OPENAI_API_KEY')
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
     logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
     # openai_log = "debug"
-    
+
     chat_engine, callback_manager, token_counter = set_up_chatbot()
 
-    try: 
+    try:
         document = AITextDocument("test2.txt", "gpt-3.5-turbo", callback_manager)
         chat_engine.add_document(document)
 
@@ -287,11 +305,14 @@ if __name__ == "__main__":
 
     questions = [
         "Where did Einstein live?",
-        "What did he work on?", # does memory work (-> he)?
-        "What was his favorite food?", # correct response: There is no information provided in the context about Einstein's favorite food.
+        "What did he work on?",  # does memory work (-> he)?
+        # correct response: There is no information provided in the context
+        # about Einstein's favorite food.
+        "What was his favorite food?",
     ]
     for question in questions:
         response = chat_engine.chat_engine.chat(question)
         print(response.response)
-        logging.info(f"Number of used tokens: {token_counter.total_embedding_token_count}")
-
+        logging.info(
+            f"Number of used tokens: {token_counter.total_embedding_token_count}"
+        )
